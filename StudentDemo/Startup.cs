76df7feb2a952fcd2662686218b4da982169a852
old-man <jdx982909501@163.com>
2020-03-27 17:55:51 +0800

    修改页面

--- StudentDemo/Startup.cs
+++ StudentDemo/Startup.cs
@@ -19,22 +19,43 @@ namespace StudentDemo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            //静态文件中间件
-            app.UseStaticFiles();
-            
+
+            //DefaultFilesOptions defaultFiles = new DefaultFilesOptions();
+            //defaultFiles.DefaultFileNames.Clear();
+            //defaultFiles.DefaultFileNames.Add("err.html");
+
+            FileServerOptions fileServerOptions = new FileServerOptions();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("err.html");
+            //UseFileServer结合了UseStaticFiles ，UseDefaultFiles ， usedirectorybrowser 中间件  不推荐使用UseFileServer 容易暴露根目录等信息
+            app.UseFileServer(fileServerOptions);
+
+            #region
+            ////添加默认文件中间件 必须注册再app.UseStaticFiles();之前
+            //app.UseDefaultFiles(defaultFiles);
+            ////UseDefaultFiles 中默认支持的页面 index.html index.htm  默认   default.html default.htm
+
+            ////添加静态文件中间件
+            //app.UseStaticFiles();
+            #endregion
+
+
+
+
+            #region
             //添加中间件
             //app.Use(async (context, next) => {
             //    //logger.LogInformation("");
             //    await next();
 
             //});
-
+            #endregion
 
 
 
