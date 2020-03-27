66c15892e8f2d0265b096818296093e9e4f91a76
old-man <jdx982909501@163.com>
2020-03-27 17:28:43 +0800

    测试

--- StudentDemo/Startup.cs
+++ StudentDemo/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace StudentDemo
 {
@@ -18,12 +19,24 @@ namespace StudentDemo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            //静态文件中间件
+            app.UseStaticFiles();
+            
+            //添加中间件
+            //app.Use(async (context, next) => {
+            //    //logger.LogInformation("");
+            //    await next();
+
+            //});
+
+
+
 
             app.Run(async (context) =>
             {
