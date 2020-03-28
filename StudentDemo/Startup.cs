7ccfc0d6fc005cb891af6db621fae1640ad73d7e
old-man <jdx982909501@163.com>
2020-03-28 09:18:47 +0800

    修改底层

--- StudentDemo/Startup.cs
+++ StudentDemo/Startup.cs
@@ -21,34 +21,34 @@ namespace StudentDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //异常中间件
+            //if (env.IsDevelopment())
+            //{
+            //    ////自定义设置错误信息
+            //    //DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions();
+            //    //developerExceptionPageOptions.SourceCodeLineCount = 1;//错误信息只显示1行
+            //    //app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+            //    app.UseDeveloperExceptionPage();
+            //}
+            #region
 
             //DefaultFilesOptions defaultFiles = new DefaultFilesOptions();
             //defaultFiles.DefaultFileNames.Clear();
             //defaultFiles.DefaultFileNames.Add("err.html");
 
-            FileServerOptions fileServerOptions = new FileServerOptions();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("err.html");
-            //UseFileServer结合了UseStaticFiles ，UseDefaultFiles ， usedirectorybrowser 中间件  不推荐使用UseFileServer 容易暴露根目录等信息
-            app.UseFileServer(fileServerOptions);
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("err.html");
+            ////UseFileServer结合了UseStaticFiles ，UseDefaultFiles ， usedirectorybrowser 中间件  不推荐使用UseFileServer 容易暴露根目录等信息
+            //app.UseFileServer(fileServerOptions);
 
-            #region
             ////添加默认文件中间件 必须注册再app.UseStaticFiles();之前
             //app.UseDefaultFiles(defaultFiles);
             ////UseDefaultFiles 中默认支持的页面 index.html index.htm  默认   default.html default.htm
 
             ////添加静态文件中间件
             //app.UseStaticFiles();
-            #endregion
 
-
-
-
-            #region
             //添加中间件
             //app.Use(async (context, next) => {
             //    //logger.LogInformation("");
@@ -57,12 +57,23 @@ namespace StudentDemo
             //});
             #endregion
 
+            //自定义环境抛出异常
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }else if(env.IsStaging()||env.IsProduction()||env.IsEnvironment("UAT"))
+            {
+                app.UseExceptionHandler("/err");
+            }
+
+            //添加静态文件中间件
+            app.UseStaticFiles();
 
 
             app.Run(async (context) =>
             {
-                throw new Exception("请求发生错误");
-                await context.Response.WriteAsync("Hello World!");
+                //throw new Exception("请求发生错误");
+                await context.Response.WriteAsync("Host :" + env.EnvironmentName);
             });
         }
     }
